d0b0531a27a05b016551f206adf47ee15e4a0ff0
Martin Böh <contact@martb.dev>
2025-08-26 03:37:56 +0200

    package/realtek-r8152: 6.12 compat

--- scripts/package-build/linux-kernel/build-realtek-r8152.py
+++ scripts/package-build/linux-kernel/build-realtek-r8152.py
@@ -32,10 +32,10 @@ KERNEL_FLAVOR: str = toml_loads(defaults_file).get('kernel_flavor')
 KERNEL_SRC: str = Path.cwd().as_posix() + '/linux'
 # define variables
 PACKAGE_NAME: str = 'vyos-drivers-realtek-r8152'
-PACKAGE_VERSION: str = '2.18.1'
+PACKAGE_VERSION: str = '2.20.1'
 PACKAGE_DIR: str = f'{PACKAGE_NAME}-{PACKAGE_VERSION}'
-SOURCES_ARCHIVE: str = 'r8152-2.18.1.tar.bz2'
-SOURCES_URL: str = f'https://packages.vyos.net/source-mirror/r8152-2.18.1.tar.bz2'
+SOURCES_ARCHIVE: str = 'r8152-2.20.1.tar.gz'
+SOURCES_URL: str = f'http://ftp.uni-kl.de/pub/linux/gentoo/distfiles/69/r8152-2.20.1.tar.gz'
 
 # download sources
 sources_archive = Path(SOURCES_ARCHIVE)
